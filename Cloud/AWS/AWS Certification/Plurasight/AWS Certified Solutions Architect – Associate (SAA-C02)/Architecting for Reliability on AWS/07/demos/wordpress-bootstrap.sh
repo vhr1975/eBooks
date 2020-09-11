04d2c4b29b91f40a405c8061538298cb45ab62b0
Victor Ramirez <vramirez75@gmail.com>
2020-09-10 21:41:52 -0700

    Added Plurasigth AWS courses

--- Cloud/AWS/AWS Certification/Plurasight/AWS Certified Solutions Architect – Associate (SAA-C02)/Architecting for Reliability on AWS/07/demos/wordpress-bootstrap.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-yum update -y
-yum install -y httpd24 php56 mysql php56-mysqlnd
-service httpd start
-chkconfig httpd on
-echo "fs-56fc251e.efs.us-east-1.amazonaws.com:/ /var/www/html nfs4 nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 0 0" >> /etc/fstab
-mount -a
